1eff781e49bcaf3317253054a67c5275283ae752
Liscak Adam <1810653053@stud.fh-kufstein.ac.at>
2018-11-27 14:28:36 +0100

    Aufgabe3

--- src/Aufgabe2.java
+++ src/Aufgabe2.java
@@ -2,15 +2,20 @@ public class Aufgabe2
 {
     public static void main(String[]args)
     {
-       for (int i= 0; i<6; i+=2)
+       for (int i= 0; i<10; i+=2)
        {
-           printName();
+           printName();printAge(20);
        }
+
 
     }
     static void printName()
     {
         System.out.println("Adam Liscak");
+    }
+    static void printAge(int age)
+    {
+        System.out.println(age);
     }
 
 }
