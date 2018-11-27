b14329083ecf252c6ae704847e4f8ede4dbe7bae
Liscak Adam <1810653053@stud.fh-kufstein.ac.at>
2018-11-27 14:25:43 +0100

    Aufgabe2

---
+++ src/Aufgabe2.java
@@ -0,0 +1,16 @@
+public class Aufgabe2
+{
+    public static void main(String[]args)
+    {
+       for (int i= 0; i<6; i+=2)
+       {
+           printName();
+       }
+
+    }
+    static void printName()
+    {
+        System.out.println("Adam Liscak");
+    }
+
+}
